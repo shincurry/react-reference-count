410e53cb53d93eaede00f762083765e0ea5de8dc
ShinCurry <shincurryyang@gmail.com>
2023-01-10 21:13:51 +0800

    Initial project

---
+++ rollup.config.mjs
@@ -0,0 +1,38 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import jsx from 'acorn-jsx';
+
+import pkg from './package.json' assert { type: 'json' };
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+      name: 'react-reference-count'
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true
+    }
+  ],
+  external: [
+    'react', 'react-dom',
+  ],
+  acornInjectPlugins: [
+    jsx(),
+  ],
+  plugins: [
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+  ]
+};
+export default config;
